7782eaa348197feadd1b4f82f368725d2ff986e2
Otavio Bernardes <otavio-bernardes@outlook.com>
2022-06-23 18:37:15 -0500

    Implementação do AccountRepositoryDatabase

---
+++ src/external/repositories/account-repository-database.ts
@@ -0,0 +1,13 @@
+import { AccountRepository } from '../../use-cases/account/ports/account-repository';
+import Connection from './ports/connection';
+
+export class AccountRepositoryDatabase implements AccountRepository {
+
+    constructor(readonly connection: Connection) { }
+
+    save(account: any): Promise<any> {
+        const query = `INSERT INTO account (user_id, balance) VALUES  (${account.user_id}, ${account.balance})`
+        console.log(query)
+        return this.connection.query(query)
+    }
+}
