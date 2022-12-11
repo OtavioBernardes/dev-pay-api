ce622ec946bd051093748a5a7d77ed45cdf93e79
Otavio <otavio-bernardes@outlook.com>
2022-12-11 12:35:15 -0300

    test: add failed test to login

--- test/e2e/login.test.ts
+++ test/e2e/login.test.ts
@@ -9,15 +9,32 @@ describe("Login", () => {
             cpf: faker.random.numeric(11),
             password: "password"
         }
-        
+
         await axios.post("http://localhost:3333/api/account", user)
 
         const result = await axios.post("http://localhost:3333/api/user/login", {
             email: user.email,
             password: user.password
         })
-        
+
         expect(result.status).toEqual(201)
         expect(result.data).toHaveProperty("token")
+    })
+
+    it("should return failed to login", async () => {
+        const user = {
+            name: "John",
+            email: faker.internet.email(),
+            cpf: faker.random.numeric(11),
+            password: "password"
+        }
+
+        const result = await axios.post("http://localhost:3333/api/user/login", {
+            email: user.email,
+            password: user.password
+        }, { validateStatus: () => true })
+
+        expect(result.status).toEqual(403)
+        expect(result.data.message).toEqual("Email and Password does not match!")
     })
 })
